88ab71767ab29e69c663df1687321a083773e071
tahsintayir <tahsintayir@hotmail.com>
2022-07-23 14:32:19 +0300

    first-commit

---
+++ patika-cSharp-deneme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_cSharp_deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
